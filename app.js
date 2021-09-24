45f062d9d6007a583bb2760943a9a25f0bf126fa
Kritika <kritika.fbd2000@gmail.com>
2021-09-24 15:40:09 +0530

    Removed DS_Store

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 console.log("Hello Kritika!!");
 console.log("Hello Again!!");
 console.log("Hey!! Dou want to eat pizza?");
+=======
+console.log("Hello Kritika!!");
+>>>>>>> parent of ed32212 (Commited Again)
